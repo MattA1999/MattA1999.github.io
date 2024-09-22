c3d3f06f88ccaa7c2a60bc6fb5f9e7ee9e5ec4cf
Matthew Arnoldussen <matt.arnoldussen@outlook.com>
2024-09-21 19:47:26 -0500

    Welcome Message
    Created a welcome message that changes depending on the time of day it is viewed at.

--- site.js
+++ site.js
@@ -1,1 +1,28 @@
-alert('test')
+const hours = new Date().getHours() // get the current hour
+
+const isMorning = hours >= 4 && hours < 12 // is it morning?
+const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
+const isEvening = hours >= 17 || hours < 4 // is it evening?
+
+let welcome; //message box variable
+
+if(isMorning) { //checks time then assigns appropriate message to variable
+    welcome = "Good morning!"
+}
+else if(isAfternoon) {
+    welcome = "Good afternoon!"
+}
+else if(isEvening) {
+    welcome = "Good evening!"
+}
+
+//connect to div and display welcome message
+
+document.getElementById('welcome').innerHTML = welcome;
+
+
+
+
+
+
+//alert('test')
