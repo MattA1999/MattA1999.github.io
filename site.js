17ad20a661bed2dd3c453d26cb2ce55f9fe6b133
Matthew Arnoldussen <matt.arnoldussen@outlook.com>
2024-10-12 01:14:49 -0500

    Assignment 5 - Carousel
    Added Image Carousel

--- site.js
+++ site.js
@@ -28,4 +28,58 @@ const key = "It's a secret to everybody."
 //save to local storage
 localStorage.setItem(key, secretMessage)
 
+
+//image carousel--
+const urls = [
+    'https://images.pexels.com/photos/1454360/pexels-photo-1454360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/933964/pexels-photo-933964.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/267885/pexels-photo-267885.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1251861/pexels-photo-1251861.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1370296/pexels-photo-1370296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+].map(url => { (new Image()).src = url; return url })
+
+const images = document.querySelectorAll('#carousel img')
+
+let currentImage = 0
+const showImages = () => {
+    const offset = currentImage % urls.length
+    images.forEach((image, index) => {
+        const imageIndex = (index + offset + urls.length) % urls.length
+        image.src = urls[imageIndex]
+    })
+
+    //increment when currentImage is called
+    currentImage++ 
+}
+
+//next button function
+const nextImage = () => {
+    currentImage++
+    showImages()
+}
+
+//prev button function
+const prevImage = () => {
+    currentImage--
+    showImages()
+}
+
+showImages()
+
+//change picture every 5 seconds
+
+setInterval(showImages, 5000)
+
+//next button event
+document.getElementById('next').addEventListener('click', () => {
+    nextImage()
+})
+
+//prev button event
+document.getElementById('prev').addEventListener('click', () =>{
+    prevImage()
+})
+
+
 //alert('test')
+
